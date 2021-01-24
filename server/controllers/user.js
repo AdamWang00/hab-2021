fb18399df0379270fe406b6752627e32fe5a4d55
Adam Wang <adam_wang1@brown.edu>
2021-01-24 10:12:47 -0500

    try catch

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -4,36 +4,44 @@ const bcrypt = require("bcrypt");
 const User = require("../models/user")
 
 exports.login = async (req, res, next) => {
-  const email = req.body.email;
-  const password = req.body.password;
+  try {
+    const email = req.body.email;
+    const password = req.body.password;
 
-  const userId = await User.login({email, password});
-  if (userId == 0) res.json({error: "Invalid Credentials"})
-  const token = jwt.sign(
-    {
-      email,
-      userId,
-    },
-    process.env.JWT_KEY
-  );
-  res.json({token, userId});
+    const userId = await User.login({email, password});
+    if (userId == 0) res.json({error: "Invalid Credentials"})
+    const token = jwt.sign(
+      {
+        email,
+        userId,
+      },
+      process.env.JWT_KEY
+    );
+    res.json({token, userId});
+  } catch (error) {
+    next(error);
+  }
 };
 
 exports.signup = async (req, res, next) => {
-  const email = req.body.email;
-  const name = req.body.name;
-  const password = req.body.password;
-  const imageUrl = req.body.imageUrl;
+  try {
+    const email = req.body.email;
+    const name = req.body.name;
+    const password = req.body.password;
+    const imageUrl = req.body.imageUrl;
 
-  // add user to db
-  const userId = await User.register({name, email, password, imageUrl});
+    // add user to db
+    const userId = await User.register({name, email, password, imageUrl});
 
-  // create jwt
-  const token = jwt.sign({
-      email,
-      userId,
-    },
-    process.env.JWT_KEY
-  );
-  res.json({token, userId});
+    // create jwt
+    const token = jwt.sign({
+        email,
+        userId,
+      },
+      process.env.JWT_KEY
+    );
+    res.json({token, userId});
+  } catch (error) {
+    next(error);
+  }
 };
