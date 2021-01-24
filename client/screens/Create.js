935c79803c74bdc6dc67660cd4483ed0aeb9d45d
tanjoshua <jtanjoshua@gmail.com>
2021-01-23 22:18:00 -0500

    type selection

--- client/screens/Create.js
+++ client/screens/Create.js
@@ -1,11 +1,49 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-
+import React, { useState } from "react";
+import { StyleSheet, Text, View, ScrollView } from "react-native";
+import { Button, TextInput, RadioButton } from "react-native-paper";
 const Create = () => {
+  const [title, setTitle] = useState("");
+  const [text, setText] = useState("");
+  const [type, setType] = useState("");
+
+  const submit = () => {
+    const body = { title, text, type };
+
+    //TODO: send request
+  };
+
   return (
-    <View>
-      <Text></Text>
-    </View>
+    <ScrollView style={{ flex: 1, padding: 20 }}>
+      <TextInput
+        label="Title"
+        style={{ marginBottom: 20 }}
+        value={title}
+        onChangeText={(value) => {
+          setTitle(value);
+        }}
+      />
+      <TextInput
+        label="Text"
+        style={{ marginBottom: 20 }}
+        multiline
+        numberOfLines={10}
+        value={text}
+        onChangeText={(value) => {
+          setText(value);
+        }}
+      />
+      <RadioButton.Group
+        style={{ marginBottom: 20 }}
+        onValueChange={(value) => setType(value)}
+        value={type}
+      >
+        <RadioButton.Item label="Post" value="post" />
+        <RadioButton.Item label="Plan" value="plan" />
+        <RadioButton.Item label="Memory" value="memory" />
+      </RadioButton.Group>
+
+      <Button onPress={submit}>Create</Button>
+    </ScrollView>
   );
 };
 
